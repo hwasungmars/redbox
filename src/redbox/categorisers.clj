9775505903012863b9b7aba6ca977038e41ad1e8
Hwasung Lee <hwasungmars@cantab.net>
2020-01-20 14:14:41 +0000

    Add classifiers.

---
+++ src/redbox/categorisers.clj
@@ -0,0 +1,28 @@
+(ns redbox.categorisers)
+
+(set! *warn-on-reflection* true)
+
+(def tag->category
+  {
+   :beauty :want
+   :book :want
+   :cafe :want
+   :commuting :need
+   :donation :want
+   :eating-out :want
+   :exercise :need
+   :groceries :need
+   :health :need
+   :household :want
+   :kids :need
+   :motors :want
+   :shopping :want
+   :technology :want
+   :travelling :want
+   })
+
+(defn categorise
+  "Given a classified map, categorise it based on tag."
+  [{:keys [tag] :as classified-map}]
+  (assoc classified-map :category (tag->category tag tag)))
+
